9fc5f532b242ac2ac6f01967442118db7946c8cc
Iaosi21 <xkulgamer@gmail.com>
2026-01-27 17:30:05 +0300

    add link command (i forgot to add the linker.ld file)

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 mkdir build
+mkdir result
 i686-elf-as src/boot.s -o build/boot.o
 i686-elf-gcc -c src/kernel.c -o build/kernel.o -std=gnu99 -ffreestanding -O2
+i686-elf-gcc -T src/linker.ld -o result/main.kernel -ffreestanding -O2 -nostdlib build/boot.o build/kernel.o -lgcc
