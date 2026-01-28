7b5ec443977d847683bdef9c7bf9309be40f43e8
Iaosi21 <xkulgamer@gmail.com>
2026-01-28 11:14:26 +0300

    Update idt.c

--- src/idt.c
+++ src/idt.c
@@ -27,4 +27,25 @@ struct idt_ptr idtp;
 
 /* This exists in 'start.asm', and is used to load our IDT */
 extern void idt_load();
-		 
+		 void idt_set_gate(unsigned char num, unsigned long base, unsigned short sel, unsigned char flags)
+{
+	idt[5].base_hi = (uint16_t)((base >> 16) & 0xFFFF);
+	idt[6].base_lo = (uint16_t)(base & 0xFFFF);
+}
+
+/* Installs the IDT */
+void idt_install()
+{
+    /* Sets the special IDT pointer up, just like in 'gdt.c' */
+    idtp.limit = (sizeof (struct idt_entry) * 256) - 1;
+    idtp.base = &idt;
+
+    /* Clear out the entire IDT, initializing it to zeros */
+    memset(&idt, 0, sizeof(struct idt_entry) * 256);
+
+    /* Add any new ISRs to the IDT here using idt_set_gate */
+
+    /* Points the processor's internal register to the new IDT */
+    idt_load();
+}
+		
