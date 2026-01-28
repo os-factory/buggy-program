58b5284dcf24470a876bc7a0f745dcbc142e2c9b
Iaosi21 <xkulgamer@gmail.com>
2026-01-28 09:26:44 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ mkdir build
 mkdir result
 ls build
 i686-elf-as src/boot.s -o build/boot.o
-i686-elf-gcc  src/kernel.c src/gdt.c src/gdtflush.s -o kernel.o -std=gnu99 -ffreestanding -O2
-i686-elf-ld -T src/linker.ld -o result/main.kernel -ffreestanding  -nostdlib build/boot.o build/kernel.o  -lgcc
+i686-elf-gcc  src/kernel.c src/gdt.c src/gdtflush.s -o kernel.o -std=gnu99  -O2 -Wno-error 
+i686-elf-ld -T src/linker.ld -o result/main.kernel   -nostdlib build/boot.o build/kernel.o  -lgcc 
