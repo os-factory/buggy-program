aba5e65a74e507af6df17cfe144a93c6254d2615
Iaosi21 <xkulgamer@gmail.com>
2026-01-28 19:36:48 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,11 +8,14 @@ i686-elf-gcc -c src/gdt.c -o build/gdt.o \
 -ffreestanding -O2 -std=gnu99
 i686-elf-gcc -c src/idt.c -o build/idt.o \
 -ffreestanding -O2 -std=gnu99
+i686-elf-gcc -c src/isrs.c -o build/isrs.o \
+-ffreestanding -O2 -std=gnu99
 
 i686-elf-as src/boot.s -o build/boot.o
 i686-elf-as src/gdtflush.s -o build/gdtflush.o 
 i686-elf-as src/idt.s -o build/idts.o
+i686-elf-as src/isrs.s -o build/isrss.o
 i686-elf-gcc -nostdlib -T src/linker.ld \
 -o result/main.kernel \
-build/boot.o build/kernel.o build/gdt.o build/gdtflush.o build/idt.o build/idts.o \
+build/boot.o build/kernel.o build/gdt.o build/gdtflush.o build/idt.o build/idts.o build/isrs.o build/isrss.o \
 -lgcc
