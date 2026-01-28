2c3c2dd7fe7e90567282b265667ea55e0617a1bb
Iaosi21 <xkulgamer@gmail.com>
2026-01-28 09:20:02 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ mkdir result
 ls build
 i686-elf-as src/boot.s -o build/boot.o
 i686-elf-gcc  src/kernel.c src/gdt.c src/gdtflush.s -o kernel.o -std=gnu99 -ffreestanding -O2
-i686-elf-gcc -T src/linker.ld -o result/main.kernel -ffreestanding -O2 -nostdlib build/boot.o build/kernel.o  -lgcc
+i686-elf-ld -T src/linker.ld -o result/main.kernel -ffreestanding  -nostdlib build/boot.o build/kernel.o  -lgcc
