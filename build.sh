d3bdebb935878694276a6f318c7c86b5f4ba3029
komarutac <118972777+winvistalover@users.noreply.github.com>
2026-01-28 00:43:03 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ mkdir result
 ls build
 i686-elf-as src/boot.s -o build/boot.o
 i686-elf-as src/crtn.s crti.s -o build/crt0.o
-i686-elf-gcc  src/kernel.c src/gdt.c src/gdtflush.s -o kernel.o -std=gnu99  -ffreestanding -O2 -Wno-error 
+i686-elf-gcc  src/kernel.c src/gdt.c src/gdtflush.s -o build/kernel.o -std=gnu99  -ffreestanding -O2 -Wno-error 
 i686-elf-ld -T src/linker.ld -o result/main.kernel -ffreestanding -nostdlib build/crt0.o build/boot.o build/kernel.o  -lgcc 
