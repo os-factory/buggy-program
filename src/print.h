a060ccba7bee0278339a6281359044613859ec64
komarutac <118972777+komarutac@users.noreply.github.com>
2026-01-29 09:50:19 -0600

    Update print.h

--- src/print.h
+++ src/print.h
@@ -2,6 +2,6 @@
 #include <stddef.h>
 size_t strlen(const char* str);
 void writechar(char Letter, uint16_t Attribute);
-void string(char text[], uint16_t color3);
+void writestring(char text[], uint16_t color3);
 uint16_t write(uint16_t chara, uint16_t color); 
 extern uint16_t* array;
