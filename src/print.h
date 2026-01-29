915fb7fb620d7ea49f1b858f1456dfe816dcc368
Iaosi21 <xkulgamer@gmail.com>
2026-01-29 09:24:06 +0300

    Update print.h

--- src/print.h
+++ src/print.h
@@ -1,7 +1,7 @@
 #include <stdint.h>
 #include <stddef.h>
-size_t strlen();
-uint16_t write();
-void writechar();
-void string();
-extern uint16_t* array[];
+size_t strlen(const char* str);
+void writechar(char c);
+void string(const char* str);
+uint16_t write(const char* buf, size_t len); 
+extern uint16_t* array;
