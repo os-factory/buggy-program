4c54280bed723769081fdd241c301bb6a207c02a
Iaosi21 <xkulgamer@gmail.com>
2026-01-30 10:10:03 +0300

    Update irq.c

--- src/irq.c
+++ src/irq.c
@@ -20,8 +20,15 @@ extern void irq13();
 extern void irq14();
 extern void irq15();
 
-/* This array is actually an array of function pointers. We use
-*  this to handle custom IRQ handlers for a given IRQ */
+static inline void outportb(uint16_t port, uint8_t val)
+{
+    __asm__ volatile ( "outb %b0, %w1" : : "a"(val), "Nd"(port) : "memory");
+    /* There's an outb %al, $imm8 encoding, for compile-time constant port numbers that fit in 8b. (N constraint).
+     * Wider immediate constants would be truncated at assemble-time (e.g. "i" constraint).
+     * The  outb  %al, %dx  encoding is the only option for all other cases.
+     * %1 expands to %dx because  port  is a uint16_t.  %w1 could be used if we had the port number a wider C type */
+}
+
 void *irq_routines[16] =
 {
     0, 0, 0, 0, 0, 0, 0, 0,
